a3925d8da5fcb117d20402c73bafc1f9e0b35579
ompatilm4-web <ompatilm4@gmail.com>
2026-06-05 23:30:40 +0530

    Done Routing

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from flask import Flask ,render_template ,redirect,url_for
+from flask import Flask ,render_template ,redirect,url_for,request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def home ():
-    return "Welcom to Zade Traders laUDA LE MERA "
+    if request.method  == 'POST':
+        pass
+    return render_template('Home.html')
 
+@app.route('/login',methods=['GET','POST'])
+def login ():
+    if request.method == 'POST':
+        pass
+    return render_template('login.html')
+
+@app.route('/dashboard',methods = ['GET','POST'])
+def dashboard ():
+    if request.method == 'POST':
+        pass
+    return render_template('dashboard.html')
 
 if __name__ == "__main__":
     app.run(debug=True,port=8080)
